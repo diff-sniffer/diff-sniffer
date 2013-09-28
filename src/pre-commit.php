79da975b51ec9c7310025406ca579243039e8916
Sergei Morozov <morozov@tut.by>
2013-09-28 21:37:23 +0300

    Verify, whether dependencies are installed. Updated dependencies.

--- src/pre-commit.php
+++ src/pre-commit.php
@@ -12,7 +12,17 @@
  * @license   http://mit-license.org/ MIT Licence
  * @link      http://github.com/morozov/diff-sniffer
  */
-require __DIR__ . '/../vendor/autoload.php';
+$autoload = __DIR__ . '/../vendor/autoload.php';
+
+if (!file_exists($autoload)) {
+    echo 'You must set up the project dependencies, run the following commands:'
+        . PHP_EOL . 'curl -sS https://getcomposer.org/installer | php'
+        . PHP_EOL . 'php composer.phar install'
+        . PHP_EOL;
+    exit(2);
+}
+
+require $autoload;
 
 $arguments = $_SERVER['argv'];
 array_shift($arguments);
