4b51c6ec8646d2c110b390c01216c9d4070b07d2
Sergei Morozov <morozov@tut.by>
2017-10-03 19:09:19 -0700

    Added RuntimeException class

---
+++ src/DiffSniffer/Exception/RuntimeException.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Run-time exception
+ *
+ * PHP version 5
+ *
+ * @category  DiffSniffer
+ * @package   DiffSniffer\Exception
+ * @author    Sergei Morozov <morozov@tut.by>
+ * @copyright 2017 Sergei Morozov
+ * @license   http://mit-license.org/ MIT Licence
+ * @link      http://github.com/morozov/diff-sniffer
+ */
+namespace DiffSniffer\Exception;
+
+use DiffSniffer\Exception;
+
+/**
+ * Run-time exception
+ *
+ * PHP version 5
+ *
+ * @category  DiffSniffer
+ * @package   DiffSniffer\Exception
+ * @author    Sergei Morozov <morozov@tut.by>
+ * @copyright 2017 Sergei Morozov
+ * @license   http://mit-license.org/ MIT Licence
+ * @link      http://github.com/morozov/diff-sniffer
+ */
+class RuntimeException extends \RuntimeException implements Exception
+{
+}
